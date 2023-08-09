f7567d262f340be642be2419cf314a98c0d05dd2
tiger-dev-hacker <120229630+tiger-dev-hacker@users.noreply.github.com>
2023-08-09 18:02:23 -0400

    Add files via upload
    I added some code between the third and the fourth unit test methods to convert cash to miles and miles to cash as these unit test were not working.

---
+++ RewardValueTests.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RewardValueTests {
+
+    @Test
+    void create_with_cash_value() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
+    }
+
+    @Test
+    void create_with_miles_value() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_cash_to_miles() {
+        int milesValue = 0;
+        int cashValue = 0;
+        var rewardValue = new RewardValue(cashValue);
+        milesValue = 0.0035 * cashValue;
+        System.out.println(milesValue);
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+        int milesValue = 0;
+        int cashValue = 0;
+        var rewardValue = new RewardValue(milesValue);
+        cashValue = milesValue / 0.0035;
+        System.out.println(cashValue);
+    }
+}
